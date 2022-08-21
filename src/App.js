07faaf251518752828fbb11be206e576b7c52966
Christian Darlington <bocacode@bocacodes-MacBook-Air.local>
2022-08-21 00:35:26 -0400

    temp app completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+
 
 function App() {
+  const [temperatureValue, setTemperatureValue] = useState(10)
+  const [temperatureColor, setTemperatureColor] = useState('cold')
+
+const increseTemperature = () => {
+  if(temperatureValue === 35) return
+  const newTemperature = temperatureValue + 1
+
+  if (newTemperature >= 15){
+    setTemperatureColor('warm')
+  } if (newTemperature >= 30)
+    setTemperatureColor('hot')
+  
+  setTemperatureValue(newTemperature)
+}
+
+const decreaseTemperature = () => {
+  if (temperatureValue === 0) return
+  const newTemperature = temperatureValue - 1
+
+  if (newTemperature < 30){
+    setTemperatureColor('warm')
+  } if(newTemperature < 15)
+    setTemperatureColor('cold')
+  
+  setTemperatureValue(newTemperature)
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+     <div className="temperature-display-container">
+      <div className={`temperature-display ${temperatureColor}`}>{temperatureValue}°C</div>
+     </div>
+      <div className="button-container">
+        <button onClick={() => increseTemperature() }>+</button>
+        <button onClick={() => decreaseTemperature()}>-</button>
+      </div>
+
     </div>
   );
 }
